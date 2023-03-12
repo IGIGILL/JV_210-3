42d316c475dc8399a2ad222b5c179d9f7b541e44
IGIGILL <36419788+IGIGILL@users.noreply.github.com>
2023-03-11 19:52:22 -0500

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,11 +1,12 @@
-
 public class Savings extends AccountType {
 	double IntRate;
-
-	public Savings(double IntRate) {
-		get.IntRate = IntRate;
+	
+	public Savings(double IntRate,double AcctNum, Customer patron) {
+		super(AcctNum, AcctNum, patron);
+		this.IntRate = IntRate;
 	}
-	public void intCalc(){
-		balance = (IntRate * balance) + balance; 
+
+	public void intCalc() {
+		balance = (IntRate * balance) + balance;
 	}
 }
